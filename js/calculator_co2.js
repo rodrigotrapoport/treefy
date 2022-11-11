ab09ee7560c93fde5858a8ed33ca336d23df66e3
rbasa <=>
2022-11-11 13:58:29 -0300

    mejora ux de la primer version de calculadora

--- js/calculator_co2.js
+++ js/calculator_co2.js
@@ -1,49 +1,65 @@
+// let easy = document.querySelectorAll('.easy-input');
+
+// console.log(easy)
+// easy.addEventListener('blur', ()=>{
+//     calculareasy()
+//   });
+document.querySelectorAll('.easy-input').forEach(item => {
+    item.addEventListener('blur', event => {
+        calculareasy()
+    })
+})
+document.querySelectorAll('.medium-input').forEach(item => {
+    item.addEventListener('blur', event => {
+        calcularmedium()
+    })
+})
 function calculareasy(){
-    var kwatth = document.getElementById("wattsh").value;
+    let kwatth = document.getElementById("wattsh").value;
     console.log('KiloWatt ' + kwatth);
-    var autos = document.getElementById("autos").value;
+    let autos = document.getElementById("autos").value;
     console.log('autos ' + autos);
-    var camionetas = document.getElementById("camionetas").value;
+    let camionetas = document.getElementById("camionetas").value;
     console.log('camionetas ' + autos);
-    var motos = document.getElementById("motos").value;
+    let motos = document.getElementById("motos").value;
     console.log('motos ' + motos);
-    var total = document.getElementById("total");
+    let total = document.getElementById("totalEasy");
     valorco2 = (kwatth * 0.000348 * 12) + (autos * 0.000143 * 12) + (camionetas * 0.000246 * 12) + (motos * 0.000112 * 12);
     valorco2 = valorco2.toFixed(2);
     total.innerHTML = valorco2;
 };
 function calcularmedium() {
-    var heladeras = document.getElementById("heladeras").value;
+    let heladeras = document.getElementById("heladeras").value;
     console.log('Heladeras ' + heladeras);
-    var caloventores = document.getElementById("caloventores").value;
+    let caloventores = document.getElementById("caloventores").value;
     console.log('caloventores ' + caloventores);
-    var aa = document.getElementById("aa").value;
+    let aa = document.getElementById("aa").value;
     console.log('aa ' + aa);
-    var lavaropas = document.getElementById("lavaropas").value;
+    let lavaropas = document.getElementById("lavaropas").value;
     console.log('lavaropas ' + lavaropas);
-    var pavas = document.getElementById("pavas").value;
+    let pavas = document.getElementById("pavas").value;
     console.log('pavas ' + pavas);
-    var computadoras = document.getElementById("computadoras").value;
+    let computadoras = document.getElementById("computadoras").value;
     console.log('computadoras ' + computadoras);
-    var planchas = document.getElementById("planchas").value;
+    let planchas = document.getElementById("planchas").value;
     console.log('planchas ' + planchas);
-    var aspiradoras = document.getElementById("aspiradoras").value;
+    let aspiradoras = document.getElementById("aspiradoras").value;
     console.log('aspiradoras ' + aspiradoras);
-    var hornos_electricos = document.getElementById("hornos_electricos").value;
+    let hornos_electricos = document.getElementById("hornos_electricos").value;
     console.log('hornos_electricos ' + hornos_electricos);
-    var microondas = document.getElementById("microondas").value;
+    let microondas = document.getElementById("microondas").value;
     console.log('microondas ' + microondas);
-    var tvleds = document.getElementById("tvleds").value;
+    let tvleds = document.getElementById("tvleds").value;
     console.log('tvleds ' + tvleds);
-    var total = document.getElementById("total");
+    let total = document.getElementById("totalMedium");
     valorco2 = (heladeras * 0.02255 * 12) + (caloventores * 0.08 * 12) + (aa * 0.0656 * 12) + (lavaropas * 0.0048 * 12) + (pavas * 0.0097 * 12) + (computadoras * 0.0155 * 12) + (planchas * 0.0111 * 12) + (aspiradoras * 0.0111 * 12) + (hornos_electricos * 0.0105 * 12) + (microondas * 0.0091 * 12) + (tvleds * 0.00175 * 12);
     valorco2 = valorco2.toFixed(2);
     total.innerHTML = valorco2;
 };
 
 /* document.getElementById('submit').onclick = function () {
-    var radios = document.getElementsByName("options_difficulty");
-    var selected = Array.from(radios).find(radio => radio.checked);
+    let radios = document.getElementsByName("options_difficulty");
+    let selected = Array.from(radios).find(radio => radio.checked);
     alert(selected.value);
     switch(selected.value){
         case "easy":
